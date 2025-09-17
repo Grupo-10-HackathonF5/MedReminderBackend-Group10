4b6e22ecac6fc734fd7e1c9dc7bbea0931142c28
Lin <lincarbajales@gmail.com>
2025-09-17 16:32:48 +0200

    feat: add Posology entity with CRUD operations

---
+++ src/test/java/com/hackathon/medreminder/Posology/controller/PosologyControllerTest.java
@@ -0,0 +1,5 @@
+package com.hackathon.medreminder.Posology.controller;
+
+public class PosologyControllerTest {
+    
+}
---
+++ src/test/java/com/hackathon/medreminder/Posology/entity/PosologyTest.java
@@ -0,0 +1,5 @@
+package com.hackathon.medreminder.Posology.entity;
+
+public class PosologyTest {
+    
+}
