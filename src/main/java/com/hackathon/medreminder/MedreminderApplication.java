128fe24b73d633320a75810b5771d6bff98a743d
vitaFlash <vitaliaflash@gmail.com>
2025-09-17 13:41:48 +0200

    first commit

---
+++ src/main/java/com/hackathon/medreminder/MedreminderApplication.java
@@ -0,0 +1,13 @@
+package com.hackathon.medreminder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedreminderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedreminderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hackathon/medreminder/MedreminderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackathon.medreminder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedreminderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
